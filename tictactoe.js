bebf08aa7a1c39300f3699144020d947db4a9eee
Robson <41270745+ROSS1996@users.noreply.github.com>
2022-11-05 11:42:17 -0300

    Add function to clear the fields

--- tictactoe.js
+++ tictactoe.js
@@ -10,11 +10,31 @@ for (const field of fields) {
 
 
 function changeField (element) {
-    if (turn == 0) {
+    if (turn == 0 && element.innerText == '') {
         element.innerText = 'X';
         turn = 1;
-    } else {
+    }
+    else if (turn == 1 && element.innerText == '') {
         element.innerText = 'O';
         turn = 0;
+    }
+    if (checkFields(element) == true) {
+        setTimeout( function () {clearFields(element)}, 1500);
+    }
+}
+
+function checkFields (element) {
+    for (const field of fields) {
+        if (field.innerText === '')
+        {
+            return false
+        }
+    }
+    return true
+}
+
+function clearFields (element) {
+    for (const field of fields) {
+        field.innerText = ''
     }
 }
