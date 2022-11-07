a9b89c4ae718c475ee2a3393db67cb288607612e
Robson <41270745+ROSS1996@users.noreply.github.com>
2022-11-07 15:57:13 -0300

    Implement function to check rows and diagonals. Add scoreboard

--- tictactoe.js
+++ tictactoe.js
@@ -1,7 +1,13 @@
 const fields = document.getElementsByClassName('field');
-
+const scores = document.getElementsByClassName('scoreCounter')
 const turnDOM = document.getElementById('turn')
+
 let turn = 1
+
+let player1_score = 0
+let player2_score = 0
+
+let locked = false
 
 for (const field of fields) {
     field.addEventListener('click', function () {
@@ -9,79 +15,113 @@ for (const field of fields) {
     })
 }
 
-function changeField (element) {
-    if (turn % 2 == 0 && element.innerText == '') {
+function changeField(element) {
+    if (turn % 2 != 0 && element.innerText == '' && locked == false) {
         element.innerText = 'X';
         turnDOM.innerHTML = 'Player 2 turn'
     }
-    else if (turn % 2 != 0 && element.innerText == '') {
+    else if (turn % 2 == 0 && element.innerText == '' && locked == false) {
         element.innerText = 'O';
         turnDOM.innerHTML = 'Player 1 turn'
     }
-    if (turn > 2) {
-        winner = checkRows()
-        if (winner == 'X' || winner == 'O') {
-            console.log(`Winner: ${winner}`)
-            setTimeout( function () {clearFields(element)}, 1500);
-        }
+    if (turn > 2 && locked == false) {
+        checkRows(element)
+        checkDiagonals(element, 'l')
+        checkDiagonals(element, 'r')
     }
     if (checkFields(element) == true) {
-        setTimeout( function () {clearFields(element)}, 1500);
+        setTimeout(function () { clearFields(element) }, 1500);
     }
     turn++
 }
 
-function checkFields (element) {
+function checkRows(element) {
+    for (i = 0; i < 9; i += 3) {
+        let row = []
+        for (j = 0; j < 3; j++) {
+            row.push(fields[i + j].innerText);
+            if (j == 2) {
+                checkWinner(element, row)
+            }
+        }
+    }
+}
+
+function checkDiagonals(element, side) {
+    if (side == 'l') {
+        let movements = []
+        for (i = 0; i < 9; i += 4) {
+            movements.push(fields[i].innerText)
+        }
+        checkWinner(element, movements)
+    }
+    else if (side == 'r') {
+        let movements = []
+        for (i = 8; i >= 0; i -= 4) {
+            movements.push(fields[i].innerText)
+        }
+        checkWinner(element, movements)
+    }
+}
+
+function checkWinner(element, movements) {
+    let winner = ''
+    if (movements[0] != '' && movements[0] == movements[1] && movements[1] == movements[2]) {
+        winner = movements[0]
+    }
+    if (winner == 'X') {
+        locked = true
+        turnDOM.innerHTML = `Winner: Player 1`
+        player1_score++
+        scores[0].innerText = player1_score
+        setTimeout(function () { clearFields(element) }, 1500);
+        locked = true
+    }
+    else if (winner === 'O') {
+        locked = true
+        turnDOM.innerHTML = `Winner: Player 2`
+        player2_score++
+        scores[1].innerText = player2_score
+        setTimeout(function () { clearFields(element) }, 1500);
+    }
+}
+
+/*
+function checkColumns (element) {
+    for (i = 0; i < 9; i += 3) {
+        console.log(`coluna ${i}`)
+        let columns = []
+        for (j = 0; j < 3 ; j ++) {
+            console.log(`${fields[i+j].id} : ${fields[i+j].innerText}`)
+            columns.push(fields[i+j].innerText)
+            //console.log(`I: ${i} | J: ${j}`)
+
+        }
+        console.log(`I: ${i} | J: ${j}`)
+        console.log(`Ola ${i+j}: ${columns}`)
+        if (j == 2) {
+            if (columns[0] == columns[1] && columns[1] == columns[2]) {
+                winner = row[0]
+            }
+        }
+    }
+}
+*/
+
+function checkFields(element) {
     for (const field of fields) {
-        if (field.innerText === '')
-        {
+        if (field.innerText === '') {
             return false
         }
     }
     return true
 }
 
-function clearFields (element) {
-    console.log(checkRows())
+function clearFields(element) {
     for (const field of fields) {
         field.innerText = ''
     }
     turn = 1
     turnDOM.innerHTML = 'Player 1 turn'
+    locked = false
 }
-
-function checkRows (element) {
-    for (i = 0; i < 9; i += 3) {
-        let row = []
-        for (j = 0; j < 3 ; j++) {
-            row.push(fields[i+j].innerText);
-            if (j == 2) {
-                //console.log(row)
-                if (row[0] == row[1] && row[1] == row[2]) {
-                    return `${row[0]}`
-                }
-            }
-        }
-    }
-}
-
-function checkColumns (element) {
-    for (i = 0; i < 3; i++) {
-        console.log(`coluna ${i+1}`)
-        for (j = 0; j < 9 ; j += 3) {
-            console.log(`${fields[i+j].id} : ${fields[i+j].innerText}`)
-        }
-    }
-}
-
-function checkDiagonals (element) {
-    console.log('Diagonal Direita')
-    for (i = 0; i < 9; i += 4 ) {
-        console.log(`${fields[i].id} : ${fields[i].innerText}`);
-    }
-    console.log('Diagonal Esquerda')
-    for (i = 8; i >= 0; i -= 4 ) {
-        console.log(`${fields[i].id} : ${fields[i].innerText}`);
-    }
-}
-
